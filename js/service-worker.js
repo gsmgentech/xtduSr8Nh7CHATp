822b53e8af78219abd1f3dc8866ff693044a2e03
Gen <gsmgentech@gmail.com>
2025-05-31 22:11:41 +0800

    Add files via upload

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,22 +1,24 @@
-self.addEventListener("install", event => {
+const CACHE_NAME = 'gentech-cache-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'manifest.json',
+  'icons/icon-192.png',
+  'icons/icon-512.png',
+  'css/style.css',
+  'js/script.js'
+];
+
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open("gentech-cache-v1").then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/style.css",
-        "/script.js",
-        "/icon-192.png",
-        "/icon-512.png"
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
